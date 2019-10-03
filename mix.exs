4f2021f841d53a2faf40f33e47469bb3ee1cf930
Sławomir Stępień <sst@poczta.fm>
2019-10-03 13:35:53 +0200

    mix: add support for release build
    Use MIX_ENV=prod mix release to build a release out of this project.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Ipaddr.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        ipaddr: [
+          applications: [
+            mmdb2_decoder: :permanent,
+            inets: :permanent,
+          ],
+        ],
+      ],
     ]
   end
 
