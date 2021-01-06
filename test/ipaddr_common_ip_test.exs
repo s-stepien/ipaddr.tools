d541daa77eea0bdf34e4be47e72fee5990e39372
Slawomir Stepien <sst@poczta.fm>
2021-01-06 12:19:59 +0100

    Fix 'Code Readability' indicated by credo

--- test/ipaddr_common_ip_test.exs
+++ test/ipaddr_common_ip_test.exs
@@ -7,8 +7,8 @@ defmodule IpaddrCommonIpTest do
   test "change ip to string" do
     assert ip_to_string!({127, 0, 0, 1}) == "127.0.0.1"
     assert ip_to_string({192, 168, 255, 10}) == {:ok, "192.168.255.10"}
-    assert ip_to_string({8193, 3512, 34211, 0, 0, 35374, 880, 29492}) == {:ok, "2001:db8:85a3::8a2e:370:7334"}
-    assert ip_to_decimal({127, 0, 0, 1}) == 2130706433
-    assert ip_to_decimal({8193, 18528, 18528, 0, 0, 0, 0, 34952}) == 42541956123769884636017138956568135816
+    assert ip_to_string({8193, 3512, 34_211, 0, 0, 35_374, 880, 29_492}) == {:ok, "2001:db8:85a3::8a2e:370:7334"}
+    assert ip_to_decimal({127, 0, 0, 1}) == 2_130_706_433
+    assert ip_to_decimal({8193, 18_528, 18_528, 0, 0, 0, 0, 34_952}) == 42_541_956_123_769_884_636_017_138_956_568_135_816
   end
 end
