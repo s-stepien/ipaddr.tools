6e043a2b3fcfabc5afd1bff66cbcb9b22c2b6667
Slawomir Stepien <sst@poczta.fm>
2021-01-06 13:34:13 +0100

    test: add tests for lookup_asn() and lookup_aso()

--- test/ipaddr_common_geo_test.exs
+++ test/ipaddr_common_geo_test.exs
@@ -14,4 +14,13 @@ defmodule IpaddrCommonGeoTest do
   test "change ip to country" do
     assert lookup_country({212, 77, 98, 9}) == {:ok, "Poland", true, "PL"}
   end
+
+  test "change ip to asn" do
+    assert lookup_asn("213.180.141.140") == {:ok, "AS12990"}
+  end
+
+  test "change ip to aso" do
+    assert lookup_aso("213.180.141.140") == {:ok, "Ringier Axel Springer Polska Sp. z o.o."}
+  end
+
 end
